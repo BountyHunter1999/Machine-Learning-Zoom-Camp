a2ce74cf40964b84e558c84f246e1bdb566635ed
mikeyy <adhikari.shrish1@gmail.com>
2021-12-02 18:35:22 +0545

    Q6??

--- Homeworks/Homework #9/lambda_function.py
+++ Homeworks/Homework #9/lambda_function.py
@@ -1,4 +1,54 @@
-import tflite_runtime.interpreter as tflite
-from keras_image_helper import create_preprocessor
+# import tflite_runtime.interpreter as tflite
+import numpy as np
+import tensorflow.lite as tflite
+from io import BytesIO
+from urllib import request
 
-preprocessor = create_preprocessor()
+from PIL import Image
+
+classes = ['cats', "dogs"]
+
+interpreter = tflite.Interpreter(model_path='dog-cat-classifier.tflite')
+interpreter.allocate_tensors()
+
+input_index = interpreter.get_input_details()[0]['index']
+output_index = interpreter.get_output_details()[0]['index']
+
+def download_image(url):
+    with request.urlopen(url) as resp:
+        buffer = resp.read()
+    stream = BytesIO(buffer)
+    img = Image.open(stream)
+    return img
+
+
+def prepare_image(img, target_size):
+    if img.mode != 'RGB':
+        img = img.convert('RGB')
+    img = img.resize(target_size, Image.NEAREST)
+    return img
+
+def preprocess_input(x):
+    x /= 255
+    return x
+
+
+def predict(url):
+    img = download_image(url)
+    our_img = prepare_image(img, (150, 150))
+    x = np.array(our_img, dtype='float32')
+    X = np.array([x])
+
+    X= preprocess_input(X)
+    interpreter.set_tensor(input_index, X)
+    interpreter.invoke()
+    preds = interpreter.get_tensor(output_index)
+
+    float_prediction = preds[0].tolist()
+
+    return dict(zip(classes, float_prediction))
+
+def lamda_handler(event, context):
+    url = event['url']
+    result = predict(url)
+    return result
